8e9fee5ed53bbfaaabbce1da2e016dc957022e79
Byseven <2104898527@qq.com>
2024-10-12 18:41:17 +0800

    test

--- test.py
+++ test.py
@@ -1,7 +1,39 @@
-# 测试img 转换~
-import sys,os
-sys.path.append(os.getcwd())
-from utils import img2simg
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@File    :   test.py
+@Time    :   2024/10/11 18:21:27
+@Author  :   Byseven
+@Version :   1.0
+@Github  :   https://www.github.com/Fansirsqi
+@Desc    :   测试文件
+"""
 
-proj_dir = './miui_GINKGO_V12.5.5.0.RCOCNXM_e8efe0e9bf_11.0/'
-img2simg(f'{proj_dir}system.img',f'{proj_dir}system.raw.img')
+import subprocess
+
+'zip -r -9 update.zip META-INF/ firmware-update/ boot.img system.new.dat.br system.patch.dat system.transfer.list vendor.new.dat.br vendor.patch.dat vendor.transfer.list'
+
+
+def create_flashable_package(output_file, folder_to_pack):
+    """
+    使用 7z 创建刷机包
+    :param output_file: 生成的刷机包名称（带路径）
+    :param folder_to_pack: 需要打包的文件夹路径
+    :return: None
+    """
+    try:
+        # 调用7z打包命令
+        subprocess.run(['7z', 'a', '-tzip', output_file, folder_to_pack], check=True)
+        print(f'Successfully created flashable package: {output_file}')
+    except subprocess.CalledProcessError as e:
+        print(f'Error during packaging: {e}')
+
+
+if __name__ == '__main__':
+    # 生成的刷机包名称
+    output_file = 'flashable_package.zip'
+    # 需要打包的文件夹路径，比如你的META-INF、system等目录所在的文件夹
+    folder_to_pack = './path_to_your_files'
+
+    # 调用函数创建刷机包
+    create_flashable_package(output_file, folder_to_pack)
