fa182dd01de1a1d0d756e4683d0d81836875a02d
Byseven <2104898527@qq.com>
2024-11-08 18:35:39 +0800

    更新 img2sdat.py 文件，重构主函数参数、增强输出目录创建，添加命令行参数支持，简化版本选择逻辑，提升代码可读性。

--- img2sdat.py
+++ img2sdat.py
@@ -2,75 +2,46 @@
 # -*- coding: utf-8 -*-
 # ====================================================
 #          FILE: img2sdat.py
-#       AUTHORS: xpirt - luxi78 - howellzhu
+#          AUTHORS: xpirt - luxi78 - howellzhu
 #          DATE: 2018-05-25 12:19:12 CEST
 # ====================================================
 
-from __future__ import print_function
-
 import os
 import sys
+import argparse
 import tempfile
-
 import blockimgdiff
 import sparse_img
 
+__version__ = '1.7'
 
-def main(INPUT_IMAGE, OUTDIR='.', VERSION=None, PREFIX='system'):
-    global input
 
-    __version__ = '1.7'
-
+def main(input_image, outdir='.', version=4, prefix='system'):
     if sys.hexversion < 0x02070000:
-        print >> sys.stderr, 'Python 2.7 or newer is required.'
-        try:
-            input = input()
-        except NameError:
-            pass
-        input('Press ENTER to exit...')
+        print('Python 2.7 or newer is required.', file=sys.stderr)
         sys.exit(1)
-    # else:
-    # print('img2sdat binary - version: %s\n' % __version__)
 
-    if not os.path.isdir(OUTDIR):
-        os.makedirs(OUTDIR)
+    # Create output directory
+    output_dir = os.path.join(outdir, prefix)
+    os.makedirs(output_dir, exist_ok=True)
 
-    OUTDIR = OUTDIR + '/' + PREFIX
+    # Process the sparse image
+    print(f'Processing input image: {input_image} with Android version: {version}')
+    with tempfile.NamedTemporaryFile() as temp_file:
+        image = sparse_img.SparseImage(input_image, temp_file.name, '0')
 
-    if not VERSION:
-        VERSION = 4
-        while True:
-            print("""            1. Android Lollipop 5.0
-            2. Android Lollipop 5.1
-            3. Android Marshmallow 6.0
-            4. Android Nougat 7.0/7.1/8.0/8.1
-            """)
-            try:
-                input = input()
-            except NameError:
-                pass
-            item = input('Choose system version: ')
-            if item == '1':
-                VERSION = 1
-                break
-            elif item == '2':
-                VERSION = 2
-                break
-            elif item == '3':
-                VERSION = 3
-                break
-            elif item == '4':
-                VERSION = 4
-                break
-            else:
-                return
+        # Generate output files
+        block_diff = blockimgdiff.BlockImageDiff(image, None, version)
+        block_diff.Compute(output_dir)
 
-    # Get sparse image
-    image = sparse_img.SparseImage(INPUT_IMAGE, tempfile.mkstemp()[1], '0')
+    print(f'Done! Output files are located in: {output_dir}')
 
-    # Generate output files
-    b = blockimgdiff.BlockImageDiff(image, None, VERSION)
-    b.Compute(OUTDIR)
 
-    # print('Done! Output files: %s' % os.path.dirname(OUTDIR))
-    return
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert .img file to Android .dat format with transfer list')
+    parser.add_argument('input_image', help='Input image file path')
+    parser.add_argument('--outdir', default='.', help='Output directory for generated files')
+    parser.add_argument('--version', type=int, choices=[1, 2, 3, 4], default=4, help='Android version: 1=Lollipop 5.0, 2=Lollipop 5.1, 3=Marshmallow 6.0, 4=Nougat 7.0/7.1/8.0/8.1')
+    parser.add_argument('--prefix', default='system', help="Prefix for output files (default: 'system')")
+    args = parser.parse_args()
+    main(args.input_image, args.outdir, args.version, args.prefix)
