61ef5cae16354cda628b4c3495b3dcb530c74b47
Byseven <2104898527@qq.com>
2024-11-08 18:36:09 +0800

    重构sdat2img.py，增加命令行参数解析，优化代码结构，合并相似逻辑，提升可读性和维护性。

--- sdat2img.py
+++ sdat2img.py
@@ -1,108 +1,81 @@
-from __future__ import print_function
-
 import os
 import sys
+import argparse
+from Log import Log as logger
 
-from utils import logger
+__version__ = '1.2'
+BLOCK_SIZE = 4096
 
 
-def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
-    __version__ = '1.2'
+def rangeset(src):
+    src_set = src.split(',')
+    num_set = [int(item) for item in src_set]
+    if len(num_set) != num_set[0] + 1:
+        logger.error('Error parsing rangeset:\n{}'.format(src))
+        sys.exit(1)
+    return [(num_set[i], num_set[i + 1]) for i in range(1, len(num_set), 2)]
 
+
+def parse_transfer_list_file(path):
+    with open(path, 'r') as trans_list:
+        version = int(trans_list.readline())
+        new_blocks = int(trans_list.readline())
+
+        if version >= 2:
+            trans_list.readline()
+            trans_list.readline()
+
+        commands = []
+        for line in trans_list:
+            parts = line.split(' ')
+            cmd = parts[0]
+            if cmd in ['erase', 'new', 'zero']:
+                commands.append((cmd, rangeset(parts[1])))
+            elif not cmd[0].isdigit():
+                logger.error('Invalid command "{}" in transfer list.'.format(cmd))
+                sys.exit(1)
+        return version, new_blocks, commands
+
+
+def main(transfer_list_file, new_data_file, output_image_file, buffer_size=4096 * 1024):
     if sys.hexversion < 0x02070000:
         logger.error('Python 2.7 or newer is required.')
-        input('Press ENTER to exit...')
-        sys.exit(1)
-    else:
-        logger.info('sdat2img binary - version: {}\n'.format(__version__))
-
-    def rangeset(src):
-        src_set = src.split(',')
-        num_set = [int(item) for item in src_set]
-        if len(num_set) != num_set[0] + 1:
-            logger.error('Error on parsing following data to rangeset:\n{}'.format(src))
-            sys.exit(1)
-        return tuple([(num_set[i], num_set[i + 1]) for i in range(1, len(num_set), 2)])
-
-    def parse_transfer_list_file(path):
-        with open(TRANSFER_LIST_FILE, 'r') as trans_list:
-            version = int(trans_list.readline())
-            new_blocks = int(trans_list.readline())
-
-            if version >= 2:
-                trans_list.readline()
-                trans_list.readline()
-
-            commands = []
-            for line in trans_list:
-                line = line.split(' ')
-                cmd = line[0]
-                if cmd in ['erase', 'new', 'zero']:
-                    commands.append([cmd, rangeset(line[1])])
-                elif not cmd[0].isdigit():
-                    logger.error('Command "{}" is not valid.'.format(cmd))
-                    sys.exit(1)
-            return version, new_blocks, commands
-
-    BLOCK_SIZE = 4096
-    BUFFER_SIZE = 1024 * BLOCK_SIZE
-
-    version, new_blocks, commands = parse_transfer_list_file(TRANSFER_LIST_FILE)
-
-    if version == 1:
-        logger.info('Android Lollipop 5.0 detected!\n')
-    elif version == 2:
-        logger.info('Android Lollipop 5.1 detected!\n')
-    elif version == 3:
-        logger.info('Android Marshmallow 6.x detected!\n')
-    elif version == 4:
-        logger.info('Android Nougat 7.x / Oreo 8.x detected!\n')
-    else:
-        logger.warning('Unknown Android version!\n')
-
-    if os.path.exists(OUTPUT_IMAGE_FILE):
-        logger.error('Error: the output file "{}" already exists'.format(OUTPUT_IMAGE_FILE))
-        logger.error('Remove it, rename it, or choose a different file name.')
         sys.exit(1)
 
-    with open(OUTPUT_IMAGE_FILE, 'wb') as output_img, open(NEW_DATA_FILE, 'rb') as new_data_file:
-        max_file_size = max(pair[1] for command in commands for pair in command[1]) * BLOCK_SIZE
+    version, new_blocks, commands = parse_transfer_list_file(transfer_list_file)
+    android_versions = {1: 'Android Lollipop 5.0', 2: 'Android Lollipop 5.1', 3: 'Android Marshmallow 6.x', 4: 'Android Nougat 7.x / Oreo 8.x'}
+    logger.info('{} detected!'.format(android_versions.get(version, 'Unknown Android version')))
 
-        for command in commands:
-            if command[0] == 'new':
-                for block in command[1]:
-                    begin, end = block
+    # if os.path.exists(output_image_file):
+    #     logger.error('Output file "{}" already exists. Choose a different name or delete it.'.format(output_image_file))
+    #     sys.exit(1)
+
+    max_file_size = max(block[1] for cmd, blocks in commands for block in blocks) * BLOCK_SIZE
+
+    with open(output_image_file, 'wb') as output_img, open(new_data_file, 'rb') as new_data:
+        for cmd, blocks in commands:
+            if cmd == 'new':
+                for begin, end in blocks:
                     block_count = end - begin
-                    logger.info('Copying {} blocks into position {}...'.format(block_count, begin))
                     output_img.seek(begin * BLOCK_SIZE)
-
                     while block_count > 0:
-                        read_size = min(block_count * BLOCK_SIZE, BUFFER_SIZE)
-                        new_data = new_data_file.read(read_size)
-                        output_img.write(new_data)
+                        read_size = min(block_count * BLOCK_SIZE, buffer_size)
+                        data = new_data.read(read_size)
+                        output_img.write(data)
                         block_count -= read_size // BLOCK_SIZE
 
         if output_img.tell() < max_file_size:
             output_img.truncate(max_file_size)
 
-    logger.success('Done! Output image: {}'.format(os.path.realpath(OUTPUT_IMAGE_FILE)))
+    logger.success('Done! Output image: {}'.format(os.path.realpath(output_image_file)))
 
 
 if __name__ == '__main__':
-    try:
-        TRANSFER_LIST_FILE = str(sys.argv[1])
-        NEW_DATA_FILE = str(sys.argv[2])
-    except IndexError:
-        logger.error('\nUsage: sdat2img.py <transfer_list> <system_new_file> [system_img]\n')
-        logger.error('    <transfer_list>: transfer list file')
-        logger.error('    <system_new_file>: system new dat file')
-        logger.error('    [system_img]: output system image\n\n')
-        input('Press ENTER to exit...')
-        sys.exit()
+    parser = argparse.ArgumentParser(description='Convert Android .dat/.br to .img file')
+    parser.add_argument('transfer_list', help='Transfer list file path')
+    parser.add_argument('new_data_file', help='System new dat file path')
+    parser.add_argument('output_image', nargs='?', default='system.img', help='Output system image file name')
+    parser.add_argument('--buffer-size', type=int, default=4096 * 1024, help='Buffer size in bytes (default: 4MB)')
 
-    try:
-        OUTPUT_IMAGE_FILE = str(sys.argv[3])
-    except IndexError:
-        OUTPUT_IMAGE_FILE = 'system.img'
-
-    main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE)
+    args = parser.parse_args()
+    main(args.transfer_list, args.new_data_file, args.output_image, args.buffer_size)
